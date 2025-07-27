38efa019e4a3434d044e94f8b09a5611c8ceab21
Kevin Santillana <thunderclouds013@gmail.com>
2025-07-27 10:27:41 -0500

    feat: manejo de lifetime de access token y botón de cerrar sesión

--- ocupasaludlab/settings.py
+++ ocupasaludlab/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=15),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }

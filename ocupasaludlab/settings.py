e4a369ee61f7c5c6e3ce50ff8fa17dfa2d6bff60
Kevin Santillana <thunderclouds013@gmail.com>
2025-07-26 19:57:11 -0500

    cambio de url

--- ocupasaludlab/settings.py
+++ ocupasaludlab/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://tu-frontend-en-render.onrender.com"
+    "https://ocupasaludlab-frontend.onrender.com"
 ]
 
 REST_FRAMEWORK = {

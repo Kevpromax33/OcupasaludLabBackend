6551b125c6be03fec2afbab540e10e0b2488fde2
Kevin Santillana <thunderclouds013@gmail.com>
2025-07-26 19:31:07 -0500

    Agrega CORS para frontend de render

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/', include('api.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('miapp.urls')),
 ]
